5507b7714ed2e7e8e4b370acf22253c8d8b9245f
CoderHelp <jacob.glomski@rockets.utoledo.edu>
2017-12-05 13:52:08 -0500

    EZ 1.2.1
    New features! Now you don't have to worry about the keyword hierarchy, we've fixed that. We've also added a feature where you can import a built-in ez file. All you have to do is run either ezwin_install.exe or eznix_install.sh (depending on your system). Our first ez file (help.ez) simply outputs the keyword docs when you put "call help" in whatever file you used to import help.ez. We'll be making more built-in ez files, so be sure to double-click that install file when we do!

--- eznix_install.sh
+++ eznix_install.sh
@@ -1,5 +1,8 @@
-sudo chmod +x ez.out # change perms
-sudo cp ez.out /usr/bin/ez # create a copy of the ez interpreter in the path
+sudo chmod +x ez.out                  # change perms
+sudo cp ez.out /usr/bin/ez            # create a copy of the ez interpreter in the path
+sudo mkdir /usr/share/ez/             # make our own dir
+sudo cp help.ez /usr/share/ez/help.ez # create a copy of help.ez in our desired location
+sudo rm -rf help.ez                   # then delete that file
 echo "Command: ez"
 echo "Usage: ez <ez-file>\n"
 echo "Description: "
